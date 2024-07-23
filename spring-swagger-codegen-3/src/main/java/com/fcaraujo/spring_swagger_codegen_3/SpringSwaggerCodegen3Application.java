e165e48bcb30d95b8cdf7a4aaa51123d1c61ecbc
Felipe Caetano Araujo <felipe.caetanoaraujo@gmail.com>
2024-07-23 20:10:58 -0300

    first commit

---
+++ spring-swagger-codegen-3/src/main/java/com/fcaraujo/spring_swagger_codegen_3/SpringSwaggerCodegen3Application.java
@@ -0,0 +1,13 @@
+package com.fcaraujo.spring_swagger_codegen_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwaggerCodegen3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwaggerCodegen3Application.class, args);
+	}
+
+}
---
+++ spring-swagger-codegen-3/src/test/java/com/fcaraujo/spring_swagger_codegen_3/SpringSwaggerCodegen3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcaraujo.spring_swagger_codegen_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwaggerCodegen3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
